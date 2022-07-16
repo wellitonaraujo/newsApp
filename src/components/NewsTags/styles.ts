beacc9e100ae855d8a22f6b3bfa1b17ff2a75fee
Welliton da Conceiçao de Araujo <welliton.araujo@compasso.com.br>
2022-07-16 11:47:11 -0300

    Add NewsTags component

---
+++ src/components/NewsTags/styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  list: {
+    maxHeight: 40,
+  },
+  contentContainer: {
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/feed/index.tsx
+++ src/screens/feed/index.tsx
@@ -6,22 +6,25 @@ import {getNewsFeed} from '../../redux/actions';
 import uuid from 'react-native-uuid';
 import styles from './styles';
 import { NewsArticle } from '../NewsArticle';
+import { NewsCategory } from '../../constants';
 
 export const Feed: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const backgroundColor = useColorScheme() === 'dark' ? '#000' : '#fff';
+  const [selectedCategory, setSelectedCategory] = useState(NewsCategory.sports);
 
   const {newsFeed} = useSelector((state: any) => state.feedReducer);
   const dispatch: Function = useDispatch();
 
   useEffect(() => {
-    dispatch(getNewsFeed(setIsLoading));
-  }, [dispatch]);
+    dispatch(getNewsFeed(setIsLoading, selectedCategory));
+  }, [dispatch, selectedCategory]);
 
   const handleRefresh = useCallback(() => {
-    dispatch(getNewsFeed(setIsLoading));
-  }, [dispatch]);
+    dispatch(getNewsFeed(setIsLoading, selectedCategory));
+  }, [dispatch, selectedCategory]);
 
+
+  const backgroundColor = useColorScheme() === 'dark' ? '#000' : '#fff';
   return (
     <View style={[styles.container, {backgroundColor}]}>
       <FlatList
