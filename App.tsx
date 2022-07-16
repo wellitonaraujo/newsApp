c0d073b3e091d66e4ee0e58eb7b1e24711d677ac
Welliton da Conceiçao de Araujo <welliton.araujo@compasso.com.br>
2022-07-16 11:25:11 -0300

    Fix PersistGate

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ActivityIndicator, Text} from 'react-native';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import {store, persistor} from './src/redux/store';
 import { Feed } from './src/screens/feed';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
+      <PersistGate
+        loading={<ActivityIndicator size={'large'} color={'#000'} />}
+        persistor={persistor}>
       <Feed />
+      </PersistGate>
     </Provider>
   );
 };
