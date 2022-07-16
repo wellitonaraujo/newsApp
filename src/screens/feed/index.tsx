1a9ac35bb3f6cde069976506358a88f1f96b46ce
Welliton da Conceiçao de Araujo <welliton.araujo@compasso.com.br>
2022-07-16 10:59:43 -0300

    Fix RefreshControl

--- src/screens/feed/index.tsx
+++ src/screens/feed/index.tsx
@@ -1,6 +1,6 @@
 
-import React, {useEffect} from 'react';
-import {FlatList, Text, useColorScheme, View} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {FlatList, RefreshControl, Text, useColorScheme, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {getNewsFeed} from '../../redux/actions';
 import uuid from 'react-native-uuid';
@@ -8,26 +8,32 @@ import styles from './styles';
 import { NewsArticle } from '../NewsArticle';
 
 export const Feed: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const backgroundColor = useColorScheme() === 'dark' ? '#000' : '#fff';
+
   const {newsFeed} = useSelector((state: any) => state.feedReducer);
   const dispatch: Function = useDispatch();
 
   useEffect(() => {
     dispatch(getNewsFeed());
   }, [dispatch]);
-  const backgroundColor = useColorScheme() === 'dark' ? '#000' : '#fff';
 
+  const handleRefresh = useCallback(() => {
+    dispatch(getNewsFeed(setIsLoading));
+  }, [dispatch]);
 
   return (
     <View style={[styles.container, {backgroundColor}]}>
       <FlatList
-      keyExtractor={() => uuid.v4()}
-      showsVerticalScrollIndicator={false}
-      data={newsFeed}
-      renderItem={({item, index}: any) => (
-        <NewsArticle post={item} />
-      )}
-      style={styles.list}
-    />
+        keyExtractor={() => uuid.v4()?.toString()}
+        showsVerticalScrollIndicator={false}
+        data={newsFeed}
+        renderItem={({item}: any) => <NewsArticle post={item} />}
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={handleRefresh} />
+        }
+        style={styles.list}
+      />
     </View>
   );
 };
